9f5a672b9129006709449499b5c3a3dc45ec4fd7
Vishal Gupta <170102988+alwaysvishal@users.noreply.github.com>
2024-05-29 22:07:30 +0530

    Create Question17.c

---
+++ Question17.c
@@ -0,0 +1,45 @@
+// Vishal Gupta
+// 2303510100108
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    printf("Enter the third number: ");
+    scanf("%d", &c);
+    if (a > b)
+    {
+        if (b > c)
+        {
+            printf("second number is the middle one");
+        }
+        else
+        {
+            printf("Third number is the middle one");
+        }
+    }
+    else if (b > c)
+    {
+        if (a > c)
+        {
+            printf("First number is the middle one");
+        }
+        else
+        {
+            printf("third number is the mmiddle one");
+        }
+    }
+    else
+    {
+        if (a > b)
+            printf("First number is the middle one");
+        else
+            printf("second number is the middle one");
+    }
+  return 0;
+}
