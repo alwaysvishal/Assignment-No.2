7f3684158d98f9c55ac9f0618c25947c905f8a70
Vishal Gupta <170102988+alwaysvishal@users.noreply.github.com>
2024-05-29 22:05:14 +0530

    Create Question14.c

---
+++ Question14.c
@@ -0,0 +1,26 @@
+// Vishal Gupta
+// 2303510100108
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter the first side: ");
+    scanf("%d",&a);
+    printf("Enter the second side: ");
+    scanf("%d",&b);
+    printf("Enter the third side: ");
+    scanf("%d",&c);
+    if(a==b && a==c){
+        printf("Equilateral");
+    }
+    else if(a==b || a==c || b==c){
+        printf("Isosceles ");
+    }
+    else{
+        printf("Scalane");
+    }
+    return 0;
+    
+}
