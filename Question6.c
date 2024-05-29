212973d8e874045724775cd0b73039f987992637
Vishal Gupta <170102988+alwaysvishal@users.noreply.github.com>
2024-05-29 22:00:12 +0530

    Create Question6.c

---
+++ Question6.c
@@ -0,0 +1,14 @@
+// Vishal Gupta
+// ROLL NO 2303510100108
+
+#include <stdio.h>
+
+int main()
+{
+   int a,b,c;
+   printf("Enter three number: ");
+   scanf("%d %d %d",&a,&b,&c);
+   float sum=a+b+c;
+   printf("average is %f",sum/3);
+    return 0;
+}
