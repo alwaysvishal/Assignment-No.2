b72831846f515dc42fa376f4860d2a0c7d77af73
Vishal Gupta <170102988+alwaysvishal@users.noreply.github.com>
2024-05-29 22:02:39 +0530

    Create Question10.c

---
+++ Question10.c
@@ -0,0 +1,18 @@
+// Vishal Gupta
+// 2303510100108
+
+#include <stdio.h>
+
+int main() 
+{
+    char a,d,c;
+    printf("Enter the three characters: ");
+    scanf("%c %c %c",&a,&d,&c);
+    int p=a;
+    int q=d;
+    int r=c;
+    printf("%c----%d\n",a,p);
+    printf("%c----%d\n",d,q);
+    printf("%c----%d\n",c,r);
+    return 0;
+}
