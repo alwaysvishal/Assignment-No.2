68efde957569ffb7f95874e2ef3cdad26f982c11
Vishal Gupta <170102988+alwaysvishal@users.noreply.github.com>
2024-05-29 21:58:59 +0530

    Create Question4.c

---
+++ Question4.c
@@ -0,0 +1,20 @@
+// Vishal Gupta
+// ROLL NO 2303510100108
+#include <stdio.h>
+
+int main() 
+{
+   int a,b,c;
+   printf("Enter the any number: ");
+   scanf("%d ",&a);
+   if(a>0){
+       printf("positive");
+   }
+   else if(a<0){
+       printf("negative");
+   }
+   else{
+       printf("zero");
+   }
+    return 0;
+}
