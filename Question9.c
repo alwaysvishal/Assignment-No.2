a683aaf9ef9e2520ac11a010aef58cb94f8b2cba
Vishal Gupta <170102988+alwaysvishal@users.noreply.github.com>
2024-05-29 22:01:53 +0530

    Create Question9.c

---
+++ Question9.c
@@ -0,0 +1,17 @@
+// Vishal Gupta
+// 2303510100108
+
+#include <stdio.h>
+
+int main() 
+{
+    int l,b,h;
+    printf("Enter the length of cuboid: ");
+    scanf("%d",&l);
+    printf("Enter the breadth of cuboid: ");
+    scanf("%d",&b);
+    printf("Enter the height of cuboid: ");
+    scanf("%d",&h);
+    printf("the volume of the cuboid id %d",l*b*h);
+    return 0;
+}
